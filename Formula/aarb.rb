5d4f869bfaba953b7db3b241d71a7920711e5087
Craig Russell <CDRussell@users.noreply.github.com>
2022-12-23 16:56:47 +0000

    Initial setup

---
+++ Formula/aarb.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Aarb < Formula
+  desc "aarb"
+  homepage "https://github.com/CDRussell/homebrew-aarb"
+  url "https://github.com/CDRussell/homebrew-aarb/releases/download/0.1.0/aarb.tar.gz"  
+  sha256 "ce7988f58540574dada623531dd96e247987c8656e86932e39c6002e619862f6"
+  version "0.1.0"
+
+  def install
+    bin.install "aarb"
+  end
+end
