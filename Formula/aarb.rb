640a9dafea13438a6b4e165a4e252f0478753411
Craig Russell <CDRussell@users.noreply.github.com>
2022-12-23 17:38:40 +0000

    Initial setup

--- Formula/aarb.rb
+++ Formula/aarb.rb
@@ -5,7 +5,7 @@ class Aarb < Formula
   desc "aarb"
   homepage "https://github.com/CDRussell/homebrew-aarb"
   url "https://github.com/CDRussell/homebrew-aarb/releases/download/0.1.0/aarb.tar.gz"  
-  sha256 "ce7988f58540574dada623531dd96e247987c8656e86932e39c6002e619862f6"
+  sha256 "b9f27a7aa8fe2bf67f64b6cfa5c0200a6ba9e94e1f2da2a80f0a9d361e5d5ddb"  
   version "0.1.0"
 
   def install
