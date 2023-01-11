cbf2c99b9ee794566f0ef01e9a5c08c2fbd5dd6c
Craig Russell <CDRussell@users.noreply.github.com>
2023-01-11 17:24:37 +0000

    Initial setup

--- Formula/aarb.rb
+++ Formula/aarb.rb
@@ -12,7 +12,7 @@ class Aarb < Formula
     #ENV["HOMEBREW_TEMP_CDR"] = "hello"
     
     libexec.install Dir["*"]
-    bin.write_exec_script (libexec/"bin/aarb")
+    bin.write_exec_script (libexec/"bin/AndroidAsanaBridge")
 
     #bin.install Dir["*"]
     #bin.install "bin"
