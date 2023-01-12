54fe64f356c5d898fe50ed28bfe09eef136ba777
Craig Russell <CDRussell@users.noreply.github.com>
2023-01-12 09:49:07 +0000

    Initial setup

--- Formula/aarb.rb
+++ Formula/aarb.rb
@@ -13,9 +13,6 @@ class Aarb < Formula
     libexec.install Dir["*"]
     bin.write_exec_script (libexec/"bin/AndroidAsanaBridge")
 
-    system "exec" " export CDR=hiFromScript"
-    system "exec" " echo $CDR"
-
     postflight do
           system_command 'echo heyhey', args: ["hi from script"]
       end
