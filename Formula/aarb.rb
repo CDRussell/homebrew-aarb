f5a593b87448bf52f0e23a326aa72ba4cd604b36
Craig Russell <1336281+CDRussell@users.noreply.github.com>
2025-01-06 14:31:37 +0000

    Update to 0.0.3

--- Formula/aarb.rb
+++ Formula/aarb.rb
@@ -4,9 +4,9 @@
 class Aarb < Formula
   desc "aarb"
   homepage "https://github.com/CDRussell/homebrew-aarb"
-  url "https://github.com/CDRussell/homebrew-aarb/releases/download/0.0.2/aarb-0.0.2.tar"  
-  sha256 "1dfa3a62d564408330f7bcf966375daf0d3a9cde5fa9fbce7c6ddca69f94951a"
-  version "0.0.2"
+  url "https://github.com/CDRussell/homebrew-aarb/releases/download/0.0.3/aarb-0.0.3.tar"
+  sha256 "d1aac73a234972310e872193dcbc36b4fd04017afc55beaa79e78adfc92659e9"
+  version "0.0.3"
 
   def install
     # install everything under one single directory, then create an executable under bin/ for invoking the script.
